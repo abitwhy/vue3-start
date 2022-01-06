3d114d201daae88dc1f369945f9d766561722ebf
abitwhy <736490428@qq.com>
2022-01-06 11:20:45 +0800

    test(netwoek): verify request ways

--- src/pages/learn/network.vue
+++ src/pages/learn/network.vue
@@ -3,13 +3,35 @@
 </template>
 
 <script>
+import net from '../../network/net'
+import { posts, users } from '../../network/api/omapi'
+
 export default {
   async created() {
-    const res = await fetch('/api/users')
-    console.log('res', res)
-    fetch('/api/users').then((response) => {
-      console.log('response', response)
-    })
+    let res = await fetch('/omapi/users'),
+      data = await res.json()
+    console.log('fetch res', res)
+    console.log('fetch data', data)
+
+    res = await net.get('/omapi/users')
+    data = res.data
+    console.log('net res', res)
+    console.log('net data', data)
+
+    res = await posts.get('2', { p: 1 })
+    data = res.data
+    console.log('api get res', res)
+    console.log('api get data', data)
+
+    res = await posts.post({ userId: '1', id: '888', title: 'lol' })
+    data = res.data
+    console.log('api post res', res)
+    console.log('api post data', data)
+
+    res = await users()
+    data = res.data
+    console.log('api users res', res)
+    console.log('api users data', data)
   },
 }
 </script>
