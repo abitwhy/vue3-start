6d938863745d45d5cf1e6cfb97834dfc372c2fb1
abitwhy <736490428@qq.com>
2022-01-10 16:37:13 +0800

    build: add echarts

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 按需引入 ECharts
+import './plugins/echarts'
+
 createApp(App).use(router).use(store).mount('#app')
