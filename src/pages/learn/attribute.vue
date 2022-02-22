2307da828cc8a82b3fad60c1de5d6c9cdd21942e
abitwhy <736490428@qq.com>
2022-02-22 11:18:27 +0800

    refactor: use setup script

--- src/pages/learn/attribute.vue
+++ src/pages/learn/attribute.vue
@@ -1,24 +1,19 @@
+<script setup>
+// const { mobile } = navigator.userAgentData // 最新的 userAgentData 标准在 chrome 调试工具里还无法正常使用
+const mobile = navigator.userAgent.includes('Mobile') // 旧版方式
+const attribute = mobile ? null : 'title' // 动态参数的空值是 null
+const event = mobile ? 'touchstart' : 'mouseenter'
+</script>
+
 <template>
-  <p :[dynamic.attribute]="动态参数" v-on:[dynamic.event]="onEvent">
-    动态参数测试
-  </p>
+  <p :[attribute]="`动态参数`" @[event]="onEvent">动态参数测试</p>
 </template>
 
 <script>
 export default {
-  computed: {
-    dynamic() {
-      // const { mobile } = navigator.userAgentData // 最新的 userAgentData 标准在 chrome 调试工具里还无法正常使用
-      const mobile = navigator.userAgent.includes('Mobile') // 旧版方式
-      const attribute = mobile ? null : 'title' // 动态参数的空值是 null
-      const event = mobile ? 'touchstart' : 'mouseenter'
-
-      return { attribute, event }
-    },
-  },
   methods: {
-    onEvent(e) {
-      console.log('event type', e.type)
+    onEvent({ type }) {
+      console.log('event type', type)
     },
   },
 }
