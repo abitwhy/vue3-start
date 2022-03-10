4cc732a9b7ce307a496b34dc97bb29e705871fec
abitwhy <736490428@qq.com>
2022-03-10 10:34:26 +0800

    perf: add titleAdapter

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,11 +1,27 @@
 /**
- * 改变页面标题
+ * 设置页面标题
  *
  * @param {Router} router vue router 实例
  * @param {RouteLocationNormalized} route 当前路由实例
+ * @param {Object} option 配置
  */
-export const setPageTitle = (router, route) => {
-  const matchedTitle = getMetaTitle(route)
+export const setPageTitle = (
+  router,
+  route,
+  /**
+   * @property {function} titleAdapter? 用户自定义标题适应器回调
+   * @see {@link getMetaTitle}
+   */
+  option
+) => {
+  const defaultOption = {
+      titleAdapter: undefined, // 默认值由底层函数决定
+    },
+    { titleAdapter } = {
+      ...defaultOption,
+      ...option,
+    }
+  const matchedTitle = getMetaTitle(route, { titleAdapter })
   const homeRoute = router.getRoutes().find(({ path }) => path === '/')
   const title = matchedTitle || getMetaTitle(homeRoute)
   if (title) document.title = title
@@ -17,25 +33,49 @@ export const setPageTitle = (router, route) => {
  *
  * @param {RouteRecordNormalized} route
  * @returns {String} 标题，未找到时返回''
+ * @param {Object} option 配置
  */
-const getMetaTitle = (route) => {
+const getMetaTitle = (
+  route,
+  /**
+   * @property {function} titleAdapter? 用户自定义标题适应器回调，接受参数：(source)
+   */
+  option
+) => {
+  const defaultOption = {
+      titleAdapter: undefined,
+      //#region undefined 默认值代表：
+      // 1.默认值由本函数决定，默认值在解构赋值处提供，否则：
+      // 2.默认值由底层函数决定
+      //#endregion
+    },
+    {
+      titleAdapter = defaultTitleAdapter,
+      //#region defaultTitleAdapter 和 titleAdapter 不得同名，存在语法问题：
+      // ReferenceError: Cannot access 'titleAdapter' before initialization
+      //#endregion
+    } = {
+      ...defaultOption,
+      ...option,
+    }
   const { meta, matched } = route
-  const availableTitles = [meta.title]
+  const availableMeta = [meta]
   if (matched) {
-    availableTitles.concat(matched.reverse().map(({ meta }) => meta.title))
+    availableMeta.concat(matched.reverse().map(({ meta }) => meta))
   }
+  const availableTitles = availableMeta.map((meta) => titleAdapter(meta))
   return availableTitles.find((title) => title) || ''
 }
 
 /**
- * 从 meta 中获取标题
+ * 默认标题适应器，一种规范结构
  *
  * @param {*} meta 路由元信息
  * @see {@link https://router.vuejs.org/zh/api/#meta}
  * @param {*} option 配置
  * @returns
  */
-export const metaTitleAdapter = (meta, option) => {
+export const defaultTitleAdapter = (meta, option) => {
   const defaultOption = {
       breadcrumb: false,
     },
